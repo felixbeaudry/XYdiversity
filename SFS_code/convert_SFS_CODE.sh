c6306e661854d4f573103d20a4b3b3c9d8426a30
Josh Hough <josh.hough415@gmail.com>
2016-07-30 16:55:12 -0700

    covert_SFS_CODE

--- SFS_code/convert_SFS_CODE.sh
+++ SFS_code/convert_SFS_CODE.sh
@@ -3,18 +3,18 @@
 #SBATCH -o /home/jhough/projects/rumex/projects/Ydiversity/logs/convert-stdout_%j.txt
 #SBATCH -e /home/jhough/projects/rumex/projects/Ydiversity/logs/convert_stderr_%j.txt
 #SBATCH -J convert_SFS
-#SBATCH --mem-per-cpu=5G
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=5
 
 set -e
 set -u
 set -o pipefail
 
-#command structure: convertSFS_CODE [commands] | msstats/src/msstats >out
-# used: - for ms style output T 0 says to output sysnonymous mutations
+#see https://github.com/molpopgen/msstats
+#command structure: convertSFS CODE <input file> <option [args]> | msstats/src/msstats >out
+# used: - for ms style output; T 0 says to output only sysnonymous mutations
 
-convertSFS_CODE -m T 0 | msstats/src/.msstats > output.txt
+convertSFS_CODE SFS.out -m T 0 | msstats/src/.msstats > stats.txt
 
 # Then, using R,
-# x=read.table("output",header=T)
+# x=read.table("stats",header=T)
 # mean(x$hsm01[x$pop==0])
