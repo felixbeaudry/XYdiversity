d71d77acbe59e1b72ed71a4b2a31b07d20ade957
Josh Hough <josh.hough415@gmail.com>
2016-10-02 20:56:09 -0700

    organized folders, added temp

--- SFS_code/sfs_code.sh
+++ SFS_code/sfs_code.sh
@@ -9,29 +9,49 @@ set -e
 set -u
 set -o pipefail
 
-
-#command structure: sfs_code <Npops> <Niter> [<options> [arguments]]
-
-# Parameters used here
-# Npops:        1
-# Niter:        50
-# theta(-t):    0.001375
+# Npops:        1 #number of pops
+# Niter:        50 #iterations
+# theta(-t):    0.001375 #=1/4 autosomal theta: 0.0055/4
 # rho(-r):      0
 # ploidy(-P):   1
-# tau(-TE):     0.3 (scaled in units of P*N generations)
-# loci(-L):     2 regions, lengths: 500000, 800000 (<R> <L1> [<L2>..] [R]): R regions with lengths <L1><L2>...
-# annotate(-a): N noncoding
-# fitness(-W):  L 1 2 0 1 1 0.184 0.003 (here, gamma shape = 0.184, and s=0.01. i.e., 0.184/5000=0.003)
-# -A tells simualtion to end
-# sampsize(-n): 8 chromosomes per sample
-# popsize:(-N)  500
+# tau(-TE):     0.3 #generations scaled in units of P*N (this seems too small?)
+# loci(-L):     2 <500000> <800000> #2 regions with lengths <L1><L2>...
+# annotate(-a): N #noncoding
+# fitness(-W):  L 1 2 0 1 1 0.258 0.005 # Gamma T model. probability positive s = 0, gamma shape = 0.258 from the DFE for x-linked genes. For a selection coefficient = 0.01, the scale parameter is 0.258/(0.01 * 5000)= 0.005.
+# sampsize(-n): 8 #chromosomes per sample
+# popsize:(-N)  5000
+# -A tells simulation to end
 
-echo "Starting Job: coding regions only"
+./sfs_code 1 50 -r 0 -t 0.001375 -P 1 -TE 0.3 -L 2 500000 800000 \
+-a N -W L 1 2 0 1 1 0.184 0.003 -n 8 -N 500 -A
+# set paramters as bash variables
+aN=0.2579 #shape parameter from X-linked DFE
+s=
+lN=${aN}/${s}*5000 # scale parameter. Hold aN constant, test a range for s. 5000=5kb
+
+#############################################################################
+echo "Starting Job: Non-coding regions"
 date
 
-sfs_code 1 50 -r 0 -t 0.001375 -P 1 -TE 0.3 -L 2 500000 800000 \
--a N -W L 1 2 0 1 1 0.184 0.003 -n 8 -N 500 -A \
--o SFS.out --errfile SFS.err
+cmd="./sfs_code 1 50 -r 0 -t 0.001375 -P 1 -TE 0.3 -L 2 500000 800000 \
+-a N -W L 1 2 0 1 1 0.258 0.005 -n 8 -N 5000 -A \
+-o SFS.out --errfile SFS.err"
 
-echo "Ending Job: coding regions "
+echo $cmd
+eval $cmd
+echo "Ending Job: Non-coding regions "
 date
+
+# fitness model in SFS_code
+# -W [P <pop>] [L <locus>] [H <type> [args]] [*F <alleleFreq> \
+# [w] [T [R <minFreq> <maxFreq> [S] [A [G <gens>]] [M <maxReps>]] \
+# [F [a] <file>]]] <type> [args]
+#
+# pop=1 (only 1)
+# L=1 (locus 1)
+# type=2 (gamma T)
+# p.pos=0 (probability positive)
+# aP=1
+# lP=1 (mean aP/lP, var =aP/lP^2) <-- only used for beneficial mutations. Here, p.pos=0, so irrelevant
+# aN=0.184 (k=shape parameter)
+# lN=0.003 (theta=scale parameter) where ${lN}=0.184/($0.01*5000)
