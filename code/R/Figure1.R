ff7966647c63045a1b418ca0fd1f7df05428d7c5
Josh Hough <josh.hough415@gmail.com>
2016-11-11 17:12:24 -0800

    edits, added new figure1, R codes

--- code/R/Figure1.R
+++ code/R/Figure1.R
@@ -1,5 +1,5 @@
-Nf=100 #number of females
-Nm=100 #number of males
+Nf=1000 #number of females
+Nm=1000 #number of males
 N=Nf+Nm #total number of individuals
 #x=Nf/(Nf+Nm) #proportion of males
 
@@ -16,7 +16,7 @@ mh=0.5
 curve(((4*N*(1-x)*x*N)/(((1-x)*N*(1+f))+(x*N*(1+mh))))/N, from=0, to=1, add=T)
 
 m1=1
-curve(((4*N*(1-x)*x*N)/(((1-x)*N*(1+f))+(x*N*(1+m1))))/N, from=0, to=1, add=T, col="blue")
+curve(((4*N*(1-x)*x*N)/(((1-x)*N*(1+f))+(x*N*(1+m1))))/N, from=0, to=1, add=T)
 
 m2=2
 curve(((4*N*(1-x)*x*N)/(((1-x)*N*(1+f))+(x*N*(1+m2))))/N, from=0, to=1, add=T)
@@ -32,7 +32,7 @@ curve(((9*N*(1-x)*N*(x))/((4*N*(1-x)*(1+f))+(2*N*(x)*(1+m))))/N, from=0, to=1, l
 
 curve(((9*N*(1-x)*N*(x))/((4*N*(1-x)*(1+f))+(2*N*(x)*(1+mh))))/N, from=0, to=1, add=T)
 
-curve(((9*N*(1-x)*N*(x))/((4*N*(1-x)*(1+f))+(2*N*(x)*(1+m1))))/N, from=0, to=1, add=T, col="blue")
+curve(((9*N*(1-x)*N*(x))/((4*N*(1-x)*(1+f))+(2*N*(x)*(1+m1))))/N, from=0, to=1, add=T)
 
 curve(((9*N*(1-x)*N*(x))/((4*N*(1-x)*(1+f))+(2*N*(x)*(1+m2))))/N, from=0, to=1, add=T)
 
@@ -59,7 +59,7 @@ curve(((9*N*x*(x-1)/(2*(x-2))))/(4*N*(1-x)*x), from=0, to=1,lty=2,
       xlab=expression(italic('N'[f]/'N'[m]+'N'[f])),
       ylim=c(0,2))
 #curve(((9*Ne_0*x*(x-1)/(2*(x-2))))/(4*N*(1-x)*x), from=0, to=1, add=T)
-curve(((9*Ne_2*x*(x-1)/(2*(x-2))))/(4*N*(1-x)*x), from=0, to=1, add=T, col="blue")
+curve(((9*Ne_2*x*(x-1)/(2*(x-2))))/(4*N*(1-x)*x), from=0, to=1, add=T)
 curve(((9*Ne_05*x*(x-1)/(2*(x-2))))/(4*N*(1-x)*x), from=0, to=1, add=T)
 curve(((9*Ne_3*x*(x-1)/(2*(x-2))))/(4*N*(1-x)*x), from=0, to=1, add=T)
 curve(((9*Ne_4*x*(x-1)/(2*(x-2))))/(4*N*(1-x)*x), from=0, to=1, add=T)
@@ -70,7 +70,7 @@ text(0.8,0.25, labels="Y/A")
 
 #curve(N*(1-x)/(2)/(4*N*(1-x)*x),from=0,to=1, add=T) #Ychromosomes
 curve(Ne_0*(1-x)/(2)/(4*N*(1-x)*x),from=0,to=1, add=T, lty=2) #Ychromosomes
-curve(Ne_2*(1-x)/(2)/(4*N*(1-x)*x),from=0,to=1, add=T, col="blue") #Ychromosomes
+curve(Ne_2*(1-x)/(2)/(4*N*(1-x)*x),from=0,to=1, add=T) #Ychromosomes
 curve(Ne_3*(1-x)/(2)/(4*N*(1-x)*x),from=0,to=1, add=T) #Ychromosomes
 curve(Ne_4*(1-x)/(2)/(4*N*(1-x)*x),from=0,to=1, add=T) #Ychromosomes
 #curve(Ne_5*(1-x)/(2)/(4*N*(1-x)*x),from=0,to=1, add=T) #Ychromosomes
@@ -83,8 +83,8 @@ curve(Ne_05*(1-x)/(2)/(4*N*(1-x)*x),from=0,to=1, add=T) #Ychromosomes
 #curve((((Ne_4*(1-x)*(1+f))/2))/N, from=0, to=1, col="red", add=T)
 #curve((((Ne_5*(1-x)*(1+f))/2))/N, from=0, to=1, col="red", add=T)
 
-Ne_500=((Nm*(km-1))/((km-1)+(500/km)))+Nf
-curve(Ne_500*(1-x)/(2)/(4*N*(1-x)*x),from=0,to=1, add=T, col="red") #Ychromosomes
+#Ne_500=((Nm*(km-1))/((km-1)+(500/km)))+Nf
+#curve(Ne_500*(1-x)/(2)/(4*N*(1-x)*x),from=0,to=1, add=T, col="red") #Ychromosomes
 
 
 #Nucelotide diversity ratios (for a neutral model, assuming equal mutation rates, and theta=4Nemu)
